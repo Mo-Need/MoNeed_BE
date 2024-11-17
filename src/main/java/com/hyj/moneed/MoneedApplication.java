b355b14298ec8cfeea043c5200e4ea7a67c039ec
이수진 <ascs25@naver.com>
2024-11-17 21:04:07 +0900

    feat: Spring boot 파일 생성
    - moneed_be 프로젝트 파일 최초 생성

---
+++ src/main/java/com/hyj/moneed/MoneedApplication.java
@@ -0,0 +1,13 @@
+package com.hyj.moneed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyj/moneed/MoneedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyj.moneed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
